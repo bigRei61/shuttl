07791660294bb1b97131c3d47c987906ded92aff
“James <james.lallave@gmail.com>
2026-07-10 22:36:48 +0800

    'he5'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password'),
+            ],
+        );
+
+        User::updateOrCreate(
+            ['email' => 'admin@shuttl.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password123'),
+                'phone' => '09171234567',
+                'gender' => 'male',
+                'date_of_birth' => '1990-01-01',
+                'role' => 'admin',
+                'rating_value' => 1000.00,
+                'matches_played' => 0,
+            ],
+        );
     }
 }
